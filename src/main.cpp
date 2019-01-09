db5e9985f7971266a2b8fb6d9e53384d5c189d7a
Deve <deveee@gmail.com>
2019-01-09 22:04:50 +0100

    Detect if there is touch device available

--- src/main.cpp
+++ src/main.cpp
@@ -1886,8 +1886,6 @@ int main(int argc, char *argv[] )
         // handle all command line options that do not need (or must
         // not have) other managers initialised:
         initUserConfig();
-        
-        UserConfigParams::m_multitouch_enabled = true;
 
         CommandLine::addArgsFromUserConfig();
 
